a471852c79c79cd9cf1fefb08cda128c2ca99c8d
Emma <minestrinad@gmail.com>
2023-11-19 17:43:07 +0100

    adding git-tcp.sh

---
+++ git-acp.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+
+#write in red: echo -e "\e[31mHello World\e[0m"
+#write in green: echo -e "\e[32mHello World\e[0m"
+# set -e
+
+# Check if the path argument is provided
+if [ -z "$1" ]; then
+    echo "Usage: $0 <path_to_directory>"
+    exit 1
+fi
+
+# Navigate to the specified directory
+cd "$HOME$1" || exit
+
+# Check if the directory exists
+if [ ! -d "$HOME$1" ]; then
+    echo -e "\e[31mError: Directory not found.\e[0m"
+    exit 1
+fi
+
+# Check if the directory is empty
+if [ -z "$(ls -A $HOME$1)" ]; then
+    echo -e "\e[31mError: Directory is empty.\e[0m"
+    exit 1
+fi
+
+# Check if the directory contains Git repositories
+#if so push directly from the directory
+if [ -d "$HOME$1/.git" ]; then
+    # Prompt the user for a commit message
+    read -p "Enter a commit message for $directory (press Enter to use timestamp): " commit_message
+
+    # Use timestamp if no commit message is provided
+    if [ -z "$commit_message" ]; then
+        timestamp=$(date +"%Y-%m-%d %T")
+        commit_message="Auto commit at $timestamp"
+    fi
+
+    # Add all changes
+    git add .
+
+    # Commit changes with a default commit message
+    git commit -m "$commit_message"
+
+    # Push changes to the remote repository
+    git push        # Modify the branch name as needed
+
+    echo -e "\e[32mGit ACP script completed.\e[0m"
+    exit 1
+fi
+
+# Iterate through each subdirectory (Git repository) in the specified directory
+for repo in */; do
+    if [ -d "$repo/.git" ]; then
+        echo -e "\e[32mProcessing repository: $repo\e[0m"
+
+        # Enter the Git repository
+        cd "$repo" || exit
+
+        # Prompt the user for a commit message
+        read -p "Enter a commit message for $directory (press Enter to use timestamp): " commit_message
+
+        # Use timestamp if no commit message is provided
+        if [ -z "$commit_message" ]; then
+            timestamp=$(date +"%Y-%m-%d %T")
+            commit_message="Auto commit at $timestamp"
+        fi
+
+        # Add all changes
+        git add .
+
+        # Commit changes with a default commit message
+        git commit -m "$commit_message"
+
+        # Push changes to the remote repository
+        git push        # Modify the branch name as needed
+
+        # Move back to the parent directory
+        cd ..
+
+        echo "---------------------------------------------"
+    fi
+done
+
+echo -e "\e[32mGit ACP script completed.\e[0m"
+
+
